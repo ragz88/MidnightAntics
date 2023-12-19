0cabcebb3d02cba594f4753723770226057877b6
ErikPrinz <erikprinz88@gmail.com>
2023-12-19 14:41:08 +0200

    Progressed Arms Scripts

--- Assets/Scripts/ArmsController.cs
+++ Assets/Scripts/ArmsController.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 
 public class ArmsController : MonoBehaviour
 {
-    
-    
-    
+
+    [SerializeField] float armSpeed = 1f;
+
+
     [SerializeField] float maxForwardDistance = 1f;
     [SerializeField] float maxReverseDistance = 1f;
-    [SerializeField] float maxLeftDistance = 1f;
-    [SerializeField] float maxRightDistance = 1f;
+    [SerializeField] float maxSideDistance = 1f;
 
 
     
@@ -41,7 +43,8 @@ public class ArmsController : MonoBehaviour
     [SerializeField] float lerpAccuracy = 0.1f;
 
 
-
+    private Vector3 leftArmInitPosition;
+    private Vector3 rightArmInitPosition;
     private bool armsActive = false;
     private bool armsLerping = false;
     private Transform currentLerpPosLeft;
@@ -51,7 +54,8 @@ public class ArmsController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        leftArmInitPosition = leftArm.localPosition;
+        rightArmInitPosition = rightArm.localPosition;
     }
 
     // Update is called once per frame
@@ -77,8 +81,38 @@ public class ArmsController : MonoBehaviour
         // Main arms movement controls should only function when in Arms movement mode
         if (armsActive)
         {
+            float leftHorizontalMovement = Input.GetAxis("Horizontal");
+            float leftVerticalMovement   = Input.GetAxis("Vertical");
+            float rightHorizontalMovement  = Input.GetAxis("LookX");
+            float rightVerticalMovement  = Input.GetAxis("LookY");
+
+            MoveArm(leftArm, leftHorizontalMovement, leftVerticalMovement);
+            MoveArm(rightArm, rightHorizontalMovement, rightVerticalMovement);
 
         }
+    }
+
+
+    /// <summary>
+    /// Moves the specified arm based on the player input. Applies relevant clamps and speed adjustments.
+    /// </summary>
+    /// <param name="arm">Arm to be moved.</param>
+    /// <param name="horizontalMovement">Float representing the horizontal movement requested by the player.</param>
+    /// <param name="verticalMovement">Float representing the vertical movement requested by the player.</param>
+    void MoveArm(Transform arm, float horizontalMovement, float verticalMovement)
+    {
+        if (arm = leftArm)
+        {
+            horizontalMovement = Mathf.Clamp(horizontalMovement, leftArmInitPosition.x - maxSideDistance, leftArmInitPosition.x + maxSideDistance);
+            verticalMovement = Mathf.Clamp(verticalMovement, leftArmInitPosition.z - maxReverseDistance, leftArmInitPosition.x + maxForwardDistance);
+        }
+        else
+        {
+            horizontalMovement = Mathf.Clamp(horizontalMovement, rightArmInitPosition.x - maxSideDistance, rightArmInitPosition.x + maxSideDistance);
+            verticalMovement = Mathf.Clamp(verticalMovement, rightArmInitPosition.z - maxReverseDistance, rightArmInitPosition.x + maxForwardDistance);
+        }
+
+        
     }
 
 
